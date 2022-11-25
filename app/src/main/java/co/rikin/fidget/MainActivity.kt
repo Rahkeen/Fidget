49cb3d785525da570eb72fbec9649ca9e5b2bb24
rmarfatia <rmarfatia@pinterest.com>
2022-11-25 00:36:39 -0800

    Remaking the fidget card with some spring animations

--- app/src/main/java/co/rikin/fidget/MainActivity.kt
+++ app/src/main/java/co/rikin/fidget/MainActivity.kt
@@ -1,14 +1,20 @@
 package co.rikin.fidget
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.VectorConverter
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.gestures.forEachGesture
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -24,20 +30,24 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.changedToUp
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import co.rikin.fidget.ui.theme.DreamyGradient
 import co.rikin.fidget.ui.theme.FidgetTheme
 import co.rikin.fidget.ui.theme.Obsidian
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,16 +55,14 @@ class MainActivity : ComponentActivity() {
     setContent {
       FidgetTheme {
         Background {
-          BoxWithConstraints(
+          Box(
             modifier = Modifier
               .width(310.dp)
               .height(210.dp)
               .background(brush = DreamyGradient, shape = RoundedCornerShape(16.dp)),
             contentAlignment = Alignment.Center
           ) {
-            with(LocalDensity.current) {
-              FidgetCard(width = maxWidth.toPx(), height = maxHeight.toPx())
-            }
+            SpringyFidgetCard()
           }
         }
       }
@@ -63,10 +71,69 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun FidgetCard(
-  width: Float,
-  height: Float
-) {
+fun SpringyFidgetCard() {
+  val scope = rememberCoroutineScope()
+  val offset = remember { Animatable(Offset.Zero, Offset.VectorConverter) }
+
+  fun transformOffsetToRotationValue(maxOffset: Float, maxRotation: Float, input: Float): Float {
+    return (maxRotation / maxOffset) * input
+  }
+
+  fun Offset.transformOffsetToRotation(size: IntSize): Offset {
+    val coercedX = x.coerceIn(0F, size.width.toFloat())
+    val coercedY = y.coerceIn(0F, size.height.toFloat())
+
+    val centerXOffset = coercedX - (size.width / 2)
+    val centerYOffset = coercedY - (size.height / 2)
+
+    Log.d("Fidget", "$centerXOffset, $centerYOffset")
+
+    val transformedX = transformOffsetToRotationValue(size.width.toFloat(), 4F, centerXOffset)
+    val transformedY = transformOffsetToRotationValue(size.height.toFloat(), 10F, centerYOffset)
+
+    return Offset(transformedX, transformedY)
+  }
+
+  Box(
+    modifier = Modifier
+      .fillMaxSize()
+      .padding(2.dp)
+      .graphicsLayer {
+        rotationX = -offset.value.y
+        rotationY = offset.value.x
+      }
+      .clip(RoundedCornerShape(16.dp))
+      .background(color = Color.Black)
+      .pointerInput(Unit) {
+        forEachGesture {
+          awaitPointerEventScope {
+            var newOffset = awaitFirstDown().position.transformOffsetToRotation(size)
+//            Log.d("Fidget", "x: ${newOffset.x}, y: ${newOffset.y}")
+            scope.launch {
+              offset.animateTo(newOffset, spring)
+            }
+            do {
+              val event = awaitPointerEvent()
+              newOffset = event.changes.last().position.transformOffsetToRotation(size)
+//              Log.d("Fidget", "x: ${newOffset.x}, y: ${newOffset.y}")
+              scope.launch {
+                offset.animateTo(newOffset, spring)
+              }
+            } while (event.changes.none { it.changedToUp() })
+            scope.launch {
+              offset.animateTo(Offset.Zero, releaseSpring)
+            }
+          }
+        }
+      },
+    contentAlignment = Alignment.Center
+  ) {
+    FidgetCardContent()
+  }
+}
+
+@Composable
+fun FidgetCard() {
   var transformAngleX by remember { mutableStateOf(0f) }
   var transformAngleY by remember { mutableStateOf(0f) }
 
@@ -100,12 +167,14 @@ fun FidgetCard(
       .background(color = Color.Black)
       .pointerInput(Unit) {
         detectTransformGestures { centroid, _, _, _ ->
+          val width = size.width.toFloat()
+          val height = size.height.toFloat()
           val coercedOffset = Offset(
             x = centroid.x.coerceIn(0F, width),
             y = centroid.y.coerceIn(0F, height)
           )
           val tapOffset = translateOffsetToCenterOrigin(width, height, coercedOffset)
-          transformAngleY = calculateTransformationOutput(width, 4F, tapOffset.x)
+          transformAngleY = calculateTransformationOutput(width, 10F, tapOffset.x)
           transformAngleX = calculateTransformationOutput(height, 10F, -tapOffset.y)
         }
       },
@@ -125,7 +194,7 @@ fun CardPreview() {
         .background(color = Color.Black),
       contentAlignment = Alignment.Center
     ) {
-      BoxWithConstraints(
+      Box(
         modifier = Modifier
           .width(310.dp)
           .height(210.dp)
@@ -135,9 +204,7 @@ fun CardPreview() {
           .background(brush = DreamyGradient),
         contentAlignment = Alignment.Center
       ) {
-        with(LocalDensity.current) {
-          FidgetCard(maxWidth.toPx(), maxHeight.toPx())
-        }
+        FidgetCard()
       }
     }
   }
@@ -199,3 +266,6 @@ fun Background(content: @Composable BoxScope.() -> Unit) {
     content = content
   )
 }
+
+private val spring = spring<Offset>(stiffness = Spring.StiffnessMediumLow)
+private val releaseSpring = spring<Offset>(dampingRatio = Spring.DampingRatioHighBouncy, stiffness = 300f)
